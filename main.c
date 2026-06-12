81b2f0953bdf5094978b811378066a3371f66c10
sharior13 <sauravshrestha648@gmail.com>
2026-06-13 01:27:44 +0545

    Update main game logic to better handle word checking

--- main.c
+++ main.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include "utils.h"
 
-#define MAX_TRIES 7
 #define MAX_WRONGS 5
 int main()
 {
-	int tryCount=0, wrongCount=0, correctCount=0;
-	char word[3]= "who";
-	char letter, guessedLetters[5] = {0};
-	printf("====================== Hangman Word Guesser ======================\n");
-	printf("Press enter to play:");
-	getch();
-	while(tryCount < MAX_TRIES){
-		printf("Word: ");
-		for(int i=0; i<sizeof(word); i++){
-			if(word[i]==guessedLetters[i]){
-				printf("%c ", word[i]);
-			}
-			else{
-				printf("_ ");
-			}
-		}
-		printf("\n");
-		printf("Enter a guess: ");
-		scanf(" %c", &letter);
-		if(letter == word[correctCount]){
-			printf("Correct!\n");
-			guessedLetters[correctCount] = letter;
-			correctCount++;
-		}
-		else{
-			printf("Incorrect!\n");
-			wrongCount++;
-		}
-		tryCount++;
-		if(correctCount == sizeof(word)){
-			printf("You guessed correctly!");
-			exit(0);
-		}
-		else if(tryCount >= MAX_TRIES){
-			printf("Out of live! The correct word was %s.", word);
-		}
-	}
-	
-    return 0;
+    char word[8] = "football";
+    int tryCount=0, wrongCount=0, correctCount=0, correctFlag=0;
+	char letter, correctLetters[sizeof(word)], wrongLetters[26 - sizeof(word)];
+    for(int i=0; i<sizeof(word);i++){
+        correctLetters[i] = '_';
+    }
+    printf("====================== Hangman Word Guesser ======================\n");
+    printf("Press enter to play:");
+    getch();
+    while(1>0){
+        correctFlag = 0;
+        printf("\nWord: ");
+        for(int i=0; i<sizeof(word); i++){
+            printf("%c ", correctLetters[i]);
+        }
+        printf("\n");
+        printf("Enter a guess: ");
+        scanf(" %c", &letter);
+        for(int i=0; i<sizeof(word); i++){
+            if(letter == word[i] && letter != correctLetters[i]){
+                correctLetters[i] = word[i];
+                correctCount++;
+                correctFlag = 1;
+            }
+            else if(letter == correctLetters[i] || letter == wrongLetters[i]){
+                printf("\nAlready guessed! Try something else.");
+                correctFlag = 2;
+                break;
+            }
+            else{
+                wrongLetters[i] = letter;
+            }
+        }
+        if(correctFlag==1){
+            printf("\nCorrect guess!");
+        }
+        else if(correctFlag == 0){
+            printf("\nIncorrect!");
+            wrongCount++;
+        }
+        tryCount++;
+        if(correctCount == sizeof(word)){
+            printf("\nWord guessed successfully!");
+            exit(0);
+        }
+    	else if(wrongCount >= MAX_WRONGS){
+            printf("\nOut of lives! The correct word was %s.", word);
+            exit(0);
+        }
+    }
+	return 0;
 }
---
+++ utils.c
@@ -0,0 +1,5 @@
+#include "utils.h"
+
+int exampleFunction(){
+	return 0;
+}
